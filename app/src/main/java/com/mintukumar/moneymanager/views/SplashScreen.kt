5e730e07edf54898c9d87d10c1acc12e2c0796bd
Mintu Kumar <sainimintu34@gmail.com>
2021-11-04 01:11:42 +0530

    Splash Screen

---
+++ app/src/main/java/com/mintukumar/moneymanager/views/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.mintukumar.moneymanager.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.mintukumar.moneymanager.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+
+class SplashScreen : AppCompatActivity() {
+    private val handler = Handler()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
